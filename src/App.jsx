b570094b415e547bf6c9ef539eb1f8fcde3388f4
Tomás Matías Lecuenis <tlecuenis@gmail.com>
2023-06-07 22:09:14 -0300

    falta agregar el cambio de texto del hover diciendo: dejar de seguir

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 import './App.css'
 
 export const App = () => {
-  const userNameIntoAt = (userName) => `@${userName}`
-  
   return (
-    <>
+    <section className='app'>
       <TwitterFollowCard 
-      name="Tomás Matías Lecuenis"
-      userName="tlecuenis"
+        name="Tomás Matías Lecuenis"
+        userName="tlecuenis"
       />
-    </>
+      <TwitterFollowCard 
+        name="Miguel Ángel Durán"
+        userName="midudev"
+      />
+      <TwitterFollowCard 
+        name="Javier Milei"
+        userName="JMilei"
+      />
+    </section>
   )
 }
 
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react"
+
 export const TwitterFollowCard = ({name, userName}) => {
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing 
+    ? 'follow is-following'
+    : 'follow'
+    //para obtener imagen dinámica, se debe declarar
+    const imageSrc = `https://unavatar.io/${userName}`
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return(
 
         <article className='card-container'>
-            <img 
-                src="https://unavatar.io/tlecuenis" 
-                alt="avatar image" 
-                />
-            
-            <div className='description'>
-                <strong>{name}</strong>
-                <div className='add'>
-                <span className='twitter-at'>@{userName}</span>
-                <span className='twitter-follow'>te sigue</span>
+            <div className="spacing">
+                <img 
+                    src={imageSrc} 
+                    alt="avatar image" 
+                    />
+                <div className='description'>
+                    <strong>{name}</strong>
+                    <div className='add'>
+                        <span className='twitter-at'>@{userName}</span>
+                        <span className='twitter-follow'>{isFollowing ? 'Te sigue' : null}</span>
+                    </div>
                 </div>
             </div>
-            <button className='follow'>Seguir</button>
+            
+            <button className={buttonClassName} onClick={handleClick}>{text}</button>
         
         </article>
     )
